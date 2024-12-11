33cceeba3e1d84f65fd6134a2cf8d142c62862ee
Fidgeh <figgehellstrom20@gmail.com>
2024-12-11 08:41:48 +0100

    Commit

---
+++ arduinopassword/arduinopassword.ino
@@ -0,0 +1,29 @@
+// Mitt projekt för att skapa ett password lock
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// funktion för att skapa en ny kod
+
+void switch() {
+  if digitalRead(8 == HIGH) {
+    
+  }
+
+}
+// funktion för att skriva in och kolla om lösenordet är rätt
+
+// funktion för att öppna dörr
